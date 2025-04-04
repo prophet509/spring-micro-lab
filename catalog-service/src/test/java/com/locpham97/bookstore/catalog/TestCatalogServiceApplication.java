a206f81a3f01504314c910c0b455ba9a647e2d96
locpham <halfbloodprincess9@gmail.com>
2025-04-04 21:25:57 +0700

    Add Catalog Service

--- catalog-service/src/test/java/com/locpham97/bookstore/catalog/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/locpham97/bookstore/catalog/TestCatalogServiceApplication.java
@@ -1,11 +1,16 @@
 package com.locpham97.bookstore.catalog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@SpringBootTest
+@Import(value = TestcontainersConfiguration.class)
 public class TestCatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
